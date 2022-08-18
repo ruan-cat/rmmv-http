7275be6519b8bf0e8db037fa6534e10e1b9d91fa
阮中楠 <1219043956@qq.com>
2022-08-18 15:24:53 +0800

    nodeResolve({ browser: true }),
    使用合适的工具。

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import json from 'rollup-plugin-json';
-import resolve from '@rollup/plugin-node-resolve';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import nodePolyfills from 'rollup-plugin-polyfill-node';
 
-// rollup.config.js
 export default {
   input: 'src/main.js',
 
@@ -30,33 +28,24 @@ export default {
       compact: true,
       extend: true,
       exports: 'named',
-      globals: {
-        // http: 'require$$1',
-        // https: 'require$$2',
-        // stream: 'require$$0$1',
-        // assert: 'require$$4',
-        // zlib: 'require$$8',
-        // util: 'require$$1$1',
-        // url: 'require$$0$1',
-        // path: 'require$$1$2',
-        // fs: 'require$$6',
-      },
       intro: '/* this is a library */',
     },
   ],
 
   plugins: [
     json(),
-    resolve({
-      jsnext: true, // 该属性是指定将Node包转换为ES2015模块
-      // main 和 browser 属性将使插件决定将那些文件应用到bundle中
-      main: true, // Default: true
-      browser: true, // Default: false
-    }),
+
+    // import resolve from 'rollup-plugin-node-resolve';
+    // https://www.cnblogs.com/tugenhua0707/p/8179686.html
+    // resolve({
+    //   jsnext: true, // 该属性是指定将Node包转换为ES2015模块
+    //   // main 和 browser 属性将使插件决定将那些文件应用到bundle中
+    //   main: true, // Default: true
+    //   browser: true, // Default: false
+    // }),
+
+    nodeResolve({ browser: true }),
+
     commonjs(),
-    nodePolyfills({
-      // include: ['http'],
-      include: null,
-    }),
   ],
 };
