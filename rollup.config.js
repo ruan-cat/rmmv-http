7c1d838a0d99e2a99870e94ddc14af85935a722e
RuanZhongNan <1219043956@qq.com>
2022-08-21 11:46:31 +0800

    模仿vue-router 增加了banner

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,19 @@ import json from 'rollup-plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
+const banner = `/*!
+  * rmmv-http v${version}
+  * (c) ${new Date().getFullYear()} 阮中楠
+  * @license MIT
+  */`;
+
 // rollup.config.js
 export default {
   input: 'src/main.js',
   output: {
     file: 'bundle.js',
     format: 'cjs',
+    banner,
   },
   plugins: [json(), resolve(), commonjs()],
 };
